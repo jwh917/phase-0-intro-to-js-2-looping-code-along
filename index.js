cba7ecf0ba67984677a31cf8c952b8408ca3b48e
Julien Hnery <jwh_917@yahoo.com>
2022-01-06 14:46:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let cards = [];
+    for(let i =0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(int) {
+    while (int > 0) {
+        console.log(int);
+        int-=1;
+    }
+    console.log(int);
+}
